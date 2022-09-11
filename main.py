a185e8e7df19678799134415623d6dc409f1af4d
sandeep Pareek <spspareek@gmail.com>
2022-09-11 03:45:31 +0000

    Modified the whole structure of directory

---
+++ main.py
@@ -0,0 +1,16 @@
+import calculate
+
+print('Sandeep')
+print(calculate.x)
+y = calculate.x + 20
+z = calculate.x * 4
+print(z)
+
+
+def foo(a):
+    print(a)
+    return a
+
+
+value = foo(z)
+print(value)
--- operation.py
+++ operation.py
@@ -1,8 +1,7 @@
-import pandas
 import calculate
-
+print('Sandeep')
 print(calculate.x)
-
 y = calculate.x + 20
 z = calculate.x * 4
 print(z)
+
